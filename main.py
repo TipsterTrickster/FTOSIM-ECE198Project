835586a30d1407a7870cfb79f51a4b209ffc950f
Zack-S56 <146447968+Zack-S56@users.noreply.github.com>
2023-10-31 00:31:38 -0400

    main.py
    I just added a couple lines (with notes next to them) that initialize the timer class that I put under stats, which you can see if you run the code. It still has a fair amount of bugs, but I can figure that out in the next couple days

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ width = 1280
 height = 720
 screen = pygame.display.set_mode((width, height))
 fto = FTO(3)
-
+#not exactly sure how to format this to make a new class for each run, along with the fact that I'm putting the ability to reset the timer into the class, so one iteration can reset itself(timer is a little buggy rn)
+run1 = stats.statis()
 
 while True:
     for event in pygame.event.get():
@@ -25,7 +26,9 @@ while True:
     
     fto.display(screen)
 #    fto.rRot()
-
+    stats.statis.timer(run1,screen)
+    #not sure how important this sleep is here, but it seems to make the timer not work as well because it doesn't loop as fast, so the responsiveness for noticing a press isn't there
+    #without the sleep, the timer works relatively well besides instantly resetting the timer to zero after you finish a solve. 
     time.sleep(1)
     pygame.display.flip()
 
